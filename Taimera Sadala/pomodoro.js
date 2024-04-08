c4005bf1077b83363c252c0ca19483c2513d476a
NotS1ick <69733624+NotS1ick@users.noreply.github.com>
2024-04-08 10:38:07 +0300

    Update pomodoro.js

--- Taimera Sadala/pomodoro.js
+++ Taimera Sadala/pomodoro.js
@@ -1,177 +1,235 @@
-var interval;
-var minutes = document.getElementById('studyTime').value;
-var seconds = document.getElementById('secondsTime').value;
-var completedCycles = 0;
-var isPaused = false;
-var isBreak = false;
+let focusButton = document.getElementById("focus");
+let buttons = document.querySelectorAll(".btn");
+let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
+let time = document.getElementById("time");
+let focusTimeInput = document.getElementById("focus-time");
+let breakTimeInput = document.getElementById("break-time");
+let updateTimerButton = document.getElementById("update-timer");
+let cycleCount = 0;
+let cycleCountDisplay = document.getElementById("cycle-count");
+let isFocusTimeCompleted = false;
+let userSetFocusTime = focusTimeInput.value || "25";
+let userSetBreakTime = breakTimeInput.value || "5";
+let startTime;
+let endTime;
 
-function isValidNumber(num) {
-    return !isNaN(num) && num >= 0;
-}
+let set;
+let active = "focus";
+let count = 0;
+let paused = true;
+let minCount = 24;
+let userSetLongBreakTime = 15
+time.textContent = `${minCount + 1}:00`;
 
-document.getElementById('start').addEventListener('click', startTimer);
-document.getElementById('pause').addEventListener('click', pauseTimer);
-document.getElementById('restart').addEventListener('click', restartTimer);
+focusButton.classList.add("btn-active");
 
-function startTimer() {
-    minutes = document.getElementById('studyTime').value;
-    seconds = document.getElementById('secondsTime').value;
-    if (!isValidNumber(minutes) || !isValidNumber(seconds)) {
-        alert('Please pick a valid number for work time and seconds');
-        return;
-    }
-    this.style.display = 'none';
-    document.getElementById('pause').style.display = 'inline-block';
-    interval = setInterval(timer, 1000);
-}
-
-function pauseTimer() {
-    clearInterval(interval);
-    this.style.display = 'none';
-    document.getElementById('start').style.display = 'inline-block';
-}
-
-function restartTimer() {
-    clearInterval(interval);
-    minutes = document.getElementById('studyTime').value;
-    seconds = document.getElementById('secondsTime').value;
-    if (!isValidNumber(minutes) || !isValidNumber(seconds)) {
-        alert('Please pick a valid number for work time and seconds');
-        return;
-    }
-    document.getElementById('pause').style.display = 'none';
-    document.getElementById('start').style.display = 'inline-block';
-    interval = setInterval(timer, 1000);
-}
-
-function timer() {
-    if (seconds > 0) {
-        seconds--;
-    } else if (minutes > 0) {
-        minutes--;
-        seconds = 59;
-    } else {
-        clearInterval(interval);
-        if (isBreak) {
-            completedCycles++;
-            document.getElementById('completedCycles').innerText = completedCycles;
-            minutes = document.getElementById('studyTime').value;
-            seconds = document.getElementById('secondsTime').value;
-            if (!isValidNumber(minutes) || !isValidNumber(seconds)) {
-                alert('Please pick a valid number for work time');
-                return;
-            }
-            isBreak = false;
-        } else {
-            minutes = document.getElementById('breakTime').value;
-            seconds = document.getElementById('secondsbreak').value;
-            if (!isValidNumber(minutes) || !isValidNumber(seconds)) {
-                alert('Please pick a valid number for break time');
-                return;
-            }
-            isBreak = true;
-        }
-        interval = setInterval(timer, 1000);
-    }
-    document.getElementById('timer').innerText = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-}
-
-
-
-// Settings icon popup, lai manitu laiku
-
-var modal = document.getElementById("settingsModal");
-var btn = document.getElementById("settings");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
-    modal.style.display = "block";
-    setTimeout(function () {
-        modal.style.opacity = "1";
-        modal.querySelector(".modal-content").style.transform = "scale(1)";
-    }, 100);
-}
-
-span.onclick = function () {
-    modal.style.opacity = "0";
-    modal.querySelector(".modal-content").style.transform = "scale(0)";
-    setTimeout(function () {
-        modal.style.display = "none";
-    }, 500);
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.opacity = "0";
-        modal.querySelector(".modal-content").style.transform = "scale(0)";
-        setTimeout(function () {
-            modal.style.display = "none";
-        }, 500);
-    }
-}
-
-// cursor
-
-const cursorDot = document.querySelector("[data-cursor-dot]");
-const cursorOutline = document.querySelector("[data-cursor-outline]");
-
-window.addEventListener("mousemove", function (e) {
-
-    const posX = e.clientX;
-    const posY = e.clientY;
-
-    cursorDot.style.left = `${posX}px`;
-    cursorDot.style.top = `${posY}px`;
-
-    cursorOutline.style.left = `${posX}px`;
-    cursorOutline.style.top = `${posY}px`;
-
-    cursorOutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    }, { duration: 500, fill: "forwards" });
-});
-
-// translations
-
-var translations = {
-    lv: {
-        SMTH: "SamOvens",
-        pmt: "PomoLaiks",
-    },
-    en: {
-        SMTH: "SamOvens",
-        pmt: "PomoTime",
-    },
-    ru: {
-        SMTH: "SamOvens",
-        pmt:  "PomoTime",
-    },
+const setActiveButton = (activeButton) => {
+  buttons.forEach(button => {
+    button.classList.remove("btn-active");
+  });
+  activeButton.classList.add("btn-active");
 };
 
-function translatePage(lang) {
-    var translatableElements = document.querySelectorAll('[data-translate]');
+const appendZero = (value) => {
+  value = value < 10 ? `0${value}` : value;
+  return value;
+};
 
-    translatableElements.forEach(function (element) {
-        var key = element.getAttribute('data-translate');
-        if (translations[lang] && translations[lang][key]) {
-            var translation = translations[lang][key];
-            translation = translation.replace(/\n/g, "<br>");
-            element.innerHTML = translation;
-        }
-    });
-}
 
-var buttons = document.querySelectorAll('.language');
+const resetTimer = () => {
+  clearInterval(set);
+  paused = true;
+  if (active === "focus") {
+    minCount = userSetFocusTime;
+  } else if (active === "short") {
+    minCount = userSetBreakTime;
+  }
+  count = 0;
+  time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+  startBtn.classList.remove("hide");
+  pause.classList.remove("show");
+  reset.classList.remove("show");
+};
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        var lang = this.getAttribute('data-lang');
-        translatePage(lang);
-    });
+reset.addEventListener(
+  "click",
+  (resetTime = () => {
+    pauseTimer();
+    if (active === "focus") {
+      minCount = userSetFocusTime;
+    } else if (active === "short") {
+      minCount = userSetBreakTime;
+    }
+    count = 0;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    startBtn.classList.remove("hide");
+    pause.classList.remove("show");
+    reset.classList.remove("show");
+  })
+);
+
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-focus");
+  });
+};
+
+focusButton.addEventListener("click", () => {
+  removeFocus();
+  focusButton.classList.add("btn-focus");
+  pauseTimer();
+  active = "focus";
+  minCount = focusTimeInput.value;
+  count = 0;
+  time.textContent = `${minCount}:${appendZero(count)}`;
 });
 
-var userLang = navigator.language || navigator.userLanguage;
-userLang = userLang.substr(0, 2);
+shortBreakButton.addEventListener("click", () => {
+  active = "short";
+  removeFocus();
+  shortBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = breakTimeInput.value;
+  count = 0;
+  time.textContent = `${minCount}:${appendZero(count)}`;
+});
 
-translatePage(userLang);
+longBreakButton.addEventListener("click", () => {
+  setActiveButton(longBreakButton);
+  active = "long";
+  minCount = userSetLongBreakTime;
+  removeFocus();
+  longBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+updateTimerButton.addEventListener("click", () => {
+  const focusTime = parseInt(focusTimeInput.value);
+  const breakTime = parseInt(breakTimeInput.value);
+  const longBreakTime = parseInt(longBreakTimeInput.value); // New long break time input field
+
+  if (focusTime > 0 && breakTime > 0 && longBreakTime > 0) {
+    userSetFocusTime = focusTime;
+    userSetBreakTime = breakTime;
+    userSetLongBreakTime = longBreakTime; // Update the userSetLongBreakTime variable
+    active = "focus";
+    minCount = userSetFocusTime;
+    count = 0;
+    cycleCount = 0;
+    isFocusTimeCompleted = false;
+    cycleCountDisplay.textContent = `Completed cycles: ${cycleCount}`;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    focusButton.classList.add("btn-active");
+    shortBreakButton.classList.remove("btn-active");
+    longBreakButton.classList.remove("btn-active");
+  } else {
+    alert("Focus time, short break time, and long break time must be greater than 0.");
+  }
+});
+
+
+longBreakButton.addEventListener("click", () => {
+  active = "long";
+  removeFocus();
+  longBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+pause.addEventListener(
+  "click",
+  (pauseTimer = () => {
+    paused = true;
+    clearInterval(set);
+    startBtn.classList.remove("hide");
+    pause.classList.remove("show");
+    reset.classList.remove("show");
+  })
+);
+
+startBtn.addEventListener("click", () => {
+  reset.classList.add("show");
+  pause.classList.add("show");
+  startBtn.classList.add("hide");
+  startBtn.classList.remove("show");
+  if (paused) {
+    paused = false;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    setTimeout(() => {
+      set = setInterval(() => {
+        if (count == 0) {
+          if (minCount == 0) {
+            if (active === "focus") {
+              isFocusTimeCompleted = true;
+              active = "short";
+              minCount = userSetBreakTime;
+              focusButton.classList.remove("btn-active");
+              shortBreakButton.classList.add("btn-active");
+            } else if (active === "short") {
+              if (isFocusTimeCompleted) {
+                cycleCount++;
+                cycleCountDisplay.textContent = `Completed cycles: ${cycleCount}`;
+                isFocusTimeCompleted = false;
+              }
+              if (cycleCount == 2) {
+                active = "long";
+                minCount = userSetLongBreakTime;
+                cycleCount = 0;
+                shortBreakButton.classList.remove("btn-active");
+                longBreakButton.classList.add("btn-active");
+              } else {
+                active = "focus";
+                minCount = userSetFocusTime;
+                shortBreakButton.classList.remove("btn-active");
+                focusButton.classList.add("btn-active");
+              }
+            } else if (active === "long") {
+              active = "focus";
+              minCount = userSetFocusTime;
+              longBreakButton.classList.remove("btn-active");
+              focusButton.classList.add("btn-active");
+            }
+            count = 0;
+            time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+          } else {
+            minCount--;
+            count = 59;
+          }
+        } else {
+          count--;
+        }
+        time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+      }, 1000);
+      time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    }, 1000);
+  }
+});
+
+// modal
+
+var modal = document.getElementById("settings-modal");
+var btn = document.getElementById("settings-btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
